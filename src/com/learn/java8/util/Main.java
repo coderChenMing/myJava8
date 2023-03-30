e960b88eabf4a1df05d1cd012a1065309c012063
chenming1 <280433706@qq.com>
2023-03-30 18:11:44 +0800

    增加单向链表

--- src/com/learn/java8/util/Main.java
+++ src/com/learn/java8/util/Main.java
@@ -2,7 +2,8 @@ package com.learn.java8.util;
 
 public class Main {
     public static void main(String[] args) {
-        test1();
+        //test1();
+        test2();
     }
 
     public static void test1() {
@@ -10,6 +11,19 @@ public class Main {
         System.out.println(list.indexOfE(0));
         //System.out.println(list.first.item);
         //System.out.println(list.last.item);
+        System.out.println(list);
+    }
+
+    public static void test2() {
+        SingleLinkedList<Integer> list = new SingleLinkedList<>();
+        list.add(2);
+        list.add(12);
+        list.add(32);
+        list.add(23);
+        list.add(24);
+        list.remove(0);
+        list.remove(0);
+        list.remove(0);
         System.out.println(list);
     }
 }
