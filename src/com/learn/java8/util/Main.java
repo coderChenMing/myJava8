33eaa19fee2b697c441b875631f9e6317e3285b5
chenming1 <280433706@qq.com>
2023-03-30 17:33:36 +0800

    新增双向链表

---
+++ src/com/learn/java8/util/Main.java
@@ -0,0 +1,15 @@
+package com.learn.java8.util;
+
+public class Main {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
+        LinkedList<Integer> list = new LinkedList<>();
+        System.out.println(list.indexOfE(0));
+        //System.out.println(list.first.item);
+        //System.out.println(list.last.item);
+        System.out.println(list);
+    }
+}
