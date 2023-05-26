2acf44ca9ba5029c3862c113eeb29146d512a953
chenming1 <280433706@qq.com>
2023-05-26 11:20:54 +0800

    增加linkedHashMap实现fifo缓存，增加雪花算法

---
+++ src/com/learn/java8/jcf/FIFOCache.java
@@ -0,0 +1,32 @@
+package com.learn.java8.jcf;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FIFOCache<K, V> extends LinkedHashMap<K, V> {
+    private final int cacheSize;
+
+    public FIFOCache(int cacheSize) {
+        this.cacheSize = cacheSize;
+    }
+
+    // 当Entry个数超过cacheSize时，删除最老的Entry
+    // 该方法的作用是告诉Map是否要删除“最老”的Entry，
+    // 所谓最老就是当前Map中最早插入的Entry，如果该方法返回`true`，
+    // 最老的那个元素就会被删除。在每次插入新元素的之后LinkedHashMap会自动询问removeEldestEntry()是否要删除最老的元素。
+    // 这样只需要在子类中重载该方法，当元素个数超过一定数量时让removeEldestEntry()返回true，就能够实现一个固定大小的FIFO策略的缓存。
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > cacheSize;
+    }
+
+    public static void main(String[] args) {
+        FIFOCache<String, String> cache = new FIFOCache<>(2);
+        cache.put("1", "张三");
+        cache.put("2", "李四");
+        cache.put("3", "王五");
+        cache.put("4", "何六");
+        cache.put("5", "崔七");
+        System.out.println(cache);
+    }
+}
